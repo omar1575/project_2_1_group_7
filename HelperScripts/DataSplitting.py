00b84c93feb15bd73029d5bd49383edf86b80484
Maksym Chumak <maksymflash@gmail.com>
2026-01-08 16:51:24 +0100

    Removed extra columms

--- HelperScripts/DataSplitting.py
+++ HelperScripts/DataSplitting.py
@@ -5,9 +5,11 @@ from sklearn.model_selection import train_test_split
 
 FOLDER = "Data/"
 data_names = ["max.csv", "Moritz_Data.csv", "omar.csv", "robert.csv"]
+exclude_columns = ["run_id","timestamp", "DRL Algorithm Used"]
 
 def normal_split(test_size:float):
     df = pd.read_csv(os.path.join(FOLDER, "Data.csv"))
+    df = df.drop(columns=exclude_columns)
     return train_test_split(df, test_size=test_size)
 
 
@@ -17,6 +19,7 @@ def startified_split(test_size:float):
     df_test = pd.DataFrame()
     for filename in data_names:
         df = pd.read_csv(os.path.join(FOLDER, filename))
+        df = df.drop(columns=exclude_columns)
         split = train_test_split(df, test_size=test_size)
         df_train = pd.concat([df_train, split[0]])
         df_test = pd.concat([df_test, split[1]])
